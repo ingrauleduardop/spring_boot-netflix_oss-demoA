739d45f45596bb67a4438d26d7d252164b406a75
Raul Perez <raul.perez@corp.isc.cl>
2018-06-06 17:14:10 -0400

    First Configurations

---
+++ spring_boot-netflix_oss-demoA/data_mining-backend/src/main/java/com/ingrauleduardop/data_miningbackend/DataMiningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ingrauleduardop.data_miningbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class DataMiningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMiningBackendApplication.class, args);
+	}
+}
